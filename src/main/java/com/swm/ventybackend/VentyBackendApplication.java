b8b736d51c08b4bf8bfdbe48bd7e2a16bab6a170
Damnun <shmom467@hanmail.net>
2023-07-18 16:41:12 +0900

    [INIT] : First Initializing
    [INIT] : First Initializing

---
+++ src/main/java/com/swm/ventybackend/VentyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.swm.ventybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VentyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VentyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swm/ventybackend/VentyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swm.ventybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VentyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
