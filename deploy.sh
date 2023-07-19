7518ccc67cb2c38cede5a7ae31f41697cea3a92e
Damnun <shmom467@hanmail.net>
2023-07-20 03:25:42 +0900

    [FIX] deploy.sh
    [FIX] deploy.sh에 JAR 실행 권한 부여 로직 추가

--- deploy.sh
+++ deploy.sh
@@ -7,14 +7,17 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
 
+chmod +x $JAR_PATH
+
 if [ -z $CURRENT_PID ] #2
 then
   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  sudo kill -15 $CURRENT_PID
+  echo "> kill -9 $CURRENT_PID"
+  sudo kill -9 $CURRENT_PID
   sleep 5
 fi
+
 
 echo "> $JAR_PATH 배포" #3
 nohup java -jar \
