67761a6c318aa0a3f1f88e8190de9ad2186e3473
Damnun <shmom467@hanmail.net>
2023-07-27 18:20:36 +0900

    [FIX] : Spring Security CSRF 권한 에러 해결
    [FIX] : Spring Security CSRF 권한 에러 해결

--- src/main/java/com/swm/ventybackend/SecurityConfig.java
+++ src/main/java/com/swm/ventybackend/SecurityConfig.java
@@ -15,6 +15,7 @@ public class SecurityConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
                         .requestMatchers(new AntPathRequestMatcher("/**")).permitAll());
         return http.build();
--- src/main/java/com/swm/ventybackend/config/DynamoDBConfiguration.java
+++ src/main/java/com/swm/ventybackend/config/DynamoDBConfiguration.java
@@ -1,14 +1,18 @@
 package com.swm.ventybackend.config;
 
+import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DynamoDBConfiguration {
@@ -28,16 +32,44 @@ public class DynamoDBConfiguration {
 
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
-        return new DynamoDBMapper(buildAmazonDynamoDB());
+        DynamoDBMapperConfig mapperConfig = DynamoDBMapperConfig.builder()
+                .withSaveBehavior(DynamoDBMapperConfig.SaveBehavior.CLOBBER)
+                .withConsistentReads(DynamoDBMapperConfig.ConsistentReads.CONSISTENT)
+                .withTableNameOverride(null)
+                .withPaginationLoadingStrategy(DynamoDBMapperConfig.PaginationLoadingStrategy.EAGER_LOADING)
+                .build();
+        return new DynamoDBMapper(buildAmazonDynamoDB(), mapperConfig);
     }
 
-    private AmazonDynamoDB buildAmazonDynamoDB() {
+//    @Bean
+//    public DynamoDBMapper dynamoDBMapper() {
+//        return new DynamoDBMapper(buildAmazonDynamoDB());
+//    }
+
+    @Primary
+    @Bean
+    public AWSCredentialsProvider awsCredentialsProvider() {
+        return new AWSStaticCredentialsProvider(new BasicAWSCredentials(dynamodbAccessKey, dynamodbSecretKey));
+    }
+
+    @Bean
+    public AmazonDynamoDB buildAmazonDynamoDB() {
         return AmazonDynamoDBClientBuilder
                 .standard()
                 .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(dynamodbEndpoint,awsRegion))
-                .withCredentials(new AWSStaticCredentialsProvider(
-                        new BasicAWSCredentials(dynamodbAccessKey,dynamodbSecretKey)))
+                        new AwsClientBuilder.EndpointConfiguration(dynamodbEndpoint, awsRegion)
+                )
+                .withCredentials(awsCredentialsProvider())
                 .build();
     }
+//
+//    private AmazonDynamoDB buildAmazonDynamoDB() {
+//        return AmazonDynamoDBClientBuilder
+//                .standard()
+//                .withEndpointConfiguration(
+//                        new AwsClientBuilder.EndpointConfiguration(dynamodbEndpoint,awsRegion))
+//                .withCredentials(new AWSStaticCredentialsProvider(
+//                        new BasicAWSCredentials(dynamodbAccessKey,dynamodbSecretKey)))
+//                .build();
+//    }
 }
