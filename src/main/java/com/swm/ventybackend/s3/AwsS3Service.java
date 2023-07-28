8cc4f322064d90307e3baa28a364cfd540650aed
Damnun <shmom467@hanmail.net>
2023-07-29 00:48:22 +0900

    [FEAT] : Thumbnail Image Resizing
    썸네일 이미지를 생성하여 리사이징 하는 로직을 추가하였습니다.

--- src/main/java/com/swm/ventybackend/s3/AwsS3Service.java
+++ src/main/java/com/swm/ventybackend/s3/AwsS3Service.java
@@ -6,14 +6,21 @@ import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.RequiredArgsConstructor;
+import net.coobird.thumbnailator.Thumbnailator;
+import net.coobird.thumbnailator.Thumbnails;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -30,16 +37,29 @@ public class AwsS3Service {
     public List<String> uploadFile(List<MultipartFile> multipartFile) {
         List<String> fileNameList = new ArrayList<>();
 
-        // forEach 구문을 통해 multipartFile로 넘어온 파일들 하나씩 fileNameList에 추가
         multipartFile.forEach(file -> {
             String fileName = createFileName(file.getOriginalFilename());
+
+            // Thumbnails 로직 추가 (230729)
+            String thumbnailFileName = "thumbnails_" + fileName;
+            File thumbnailFile = new File(thumbnailFileName);
+            Thumbnails.Builder builder;
+            try {
+                BufferedImage orginalImage = ImageIO.read(file.getInputStream());
+                builder = Thumbnails.of(orginalImage).size(100, 100);
+                builder.toFile(thumbnailFile);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
             ObjectMetadata objectMetadata = new ObjectMetadata();
             objectMetadata.setContentLength(file.getSize());
             objectMetadata.setContentType(file.getContentType());
 
             try(InputStream inputStream = file.getInputStream()) {
-                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                amazonS3.putObject(new PutObjectRequest(bucket, "contents/" + fileName, inputStream, objectMetadata)
                         .withCannedAcl(CannedAccessControlList.PublicRead));
+                amazonS3.putObject(bucket, "thumbnails/" + thumbnailFileName, thumbnailFile);
             } catch(IOException e) {
                 throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "파일 업로드에 실패했습니다.");
             }
@@ -55,7 +75,7 @@ public class AwsS3Service {
         System.out.println("bucket = " + bucket);
     }
 
-    private String createFileName(String fileName) { // 먼저 파일 업로드 시, 파일명을 난수화하기 위해 random으로 돌립니다.
+    private String createFileName(String fileName) {
         return UUID.randomUUID().toString().concat(getFileExtension(fileName));
     }
 
