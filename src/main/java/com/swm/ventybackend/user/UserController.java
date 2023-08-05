88a844062a3483b5b72603bd8c726b7a838939f8
Damnun <shmom467@hanmail.net>
2023-08-06 02:21:02 +0900

    [FIX] : 함수명 충돌 오류 수정

--- src/main/java/com/swm/ventybackend/user/UserController.java
+++ src/main/java/com/swm/ventybackend/user/UserController.java
@@ -1,24 +1,52 @@
 package com.swm.ventybackend.user;
 
+import jakarta.annotation.Nullable;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.Timestamp;
 
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    @PersistenceContext
-    private EntityManager entityManager;
+    @PostMapping("/join")
+    public String create(@RequestParam String email, String password, String user_name,
+                         Integer gender, String nickname, Integer status) {
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+        user.setUserName(user_name);
+        user.setGender(gender);
+        user.setNickName(nickname);
+        user.setStatus(status);
 
-    @GetMapping("getAll")
-    public String searchAllUser() {
-        return userRepository.searchAllUser().toString();
+        Long userId = userService.saveUser(user);
+        return userId + "번 유저 등록 완료";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(@RequestParam Long id) {
+        userService.removeUser(id);
+        return id + "번 유저 삭제 완료";
+    }
+
+    @GetMapping("/findByIdOrName")
+    public String read(@RequestParam @Nullable Long id, String name) {
+        if(id != null) {
+            return userService.findUserById(id).toString();
+        } else {
+            return userService.findUserByName(name).toString();
+        }
+    }
+
+    @GetMapping("/all")
+    public String readAll() {
+        return userService.findAllUser().toString();
     }
 }
