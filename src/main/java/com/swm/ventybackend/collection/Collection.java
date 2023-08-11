9cd1290ddbea85d3a2eb982cf04ec30d181b144d
Damnun <shmom467@hanmail.net>
2023-08-11 14:57:48 +0900

    [FIX] Collection 테이블 FK 삭제
    [FIX] Collection 테이블 FK 삭제

--- src/main/java/com/swm/ventybackend/collection/Collection.java
+++ src/main/java/com/swm/ventybackend/collection/Collection.java
@@ -33,22 +33,14 @@ public class Collection {
     @ColumnDefault("1")
     private Integer status;
 
-    // FK 참조관계 끊고 단일로 생성
+    @Column(name = "thumbnail_image_url")
+    @ColumnDefault("null")
+    private String thumbnailImageUrl;
 
     @Column(name = "club_id")
     private Long clubId;
 
-////    @ManyToOne // Many = Collection, One = Group 하나의 그룹은 여러 컬렉션을 가질 수 있음
-//    @ManyToOne
-//    @JoinColumn(name = "club_id")
-//    private Club club;
-
-
-
     @Column(name = "users_id")
     private Long usersId;
 
-//    @OneToOne
-//    @JoinColumn(name = "users_id")
-//    private Users users;
 }
