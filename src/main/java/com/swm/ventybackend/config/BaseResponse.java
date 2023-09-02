44f1c5a189245c663dad5f4fb7f55c738ad85fcc
Damnun <shmom467@hanmail.net>
2023-09-02 21:12:36 +0900

    [FEAT] : Exception, SHA256, JWT, Validation
    [FEAT] : Exception, SHA256, JWT, Validation

---
+++ src/main/java/com/swm/ventybackend/config/BaseResponse.java
@@ -0,0 +1,37 @@
+package com.swm.ventybackend.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import static com.swm.ventybackend.config.BaseResponseStatus.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class BaseResponse<T> {
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String message;
+    private final int code;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // success
+    public BaseResponse(T result) {
+        this.isSuccess = SUCCESS.isSuccess();
+        this.message = SUCCESS.getMessage();
+        this.code = SUCCESS.getCode();
+        this.result = result;
+    }
+
+    // fail
+    public BaseResponse(BaseResponseStatus status) {
+        this.isSuccess = status.isSuccess();
+        this.message = status.getMessage();
+        this.code = status.getCode();
+    }
+}
