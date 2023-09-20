f13d61945bf0049c0fe9ebd147d2cb34754a51d5
Damnun <shmom467@hanmail.net>
2023-09-20 19:57:06 +0900

    [FEAT] : /users/updateUsersInfoByUsersId
    [FEAT] : /users/updateUsersInfoByUsersId

--- src/main/java/com/swm/ventybackend/users/UsersController.java
+++ src/main/java/com/swm/ventybackend/users/UsersController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @RestController
@@ -24,7 +25,7 @@ public class UsersController {
 
     @PostMapping("/create")
     public String create(@RequestParam String email, String password, String users_name,
-                         Integer gender, String nickname, Integer status) {
+                         String gender, String nickname, Integer status) {
         // @TODO : profileImage File로 변경 및 API 분리
         Users users = new Users();
         users.setEmail(email);
@@ -40,7 +41,41 @@ public class UsersController {
         Long cloudId = cloudService.saveCloud(cloud);
 
         return usersId + "번 유저 " + cloudId + "번 클라우드 등록 완료";
+
+        // @TODO: 디비 변동에 맞게 수정
     }
+
+    @PostMapping("/loginKakaoUser")
+    public String loginKakaoUser(@RequestParam String kakaoId, String email, String gender, String ageRange) {
+        // 3001771464, junghk0115@naver.com, Gender.female, AgeRange.age_20_29
+
+        Optional<Users> users = usersService.findUsersByOAuthIdAndType(Long.valueOf(kakaoId), "kakao");
+
+        if (users.isEmpty()) {
+            Users newUsers = new Users();
+            newUsers.setOAuthId(Long.valueOf(kakaoId));
+            newUsers.setOAuthType("kakao");
+            newUsers.setAgeRange(ageRange);
+            newUsers.setEmail(email);
+            newUsers.setGender(gender);
+
+            Long newUsersId = usersService.saveUser(newUsers);
+
+            Cloud cloud = new Cloud();
+            cloud.setUsersId(newUsersId);
+            Long cloudId = cloudService.saveCloud(cloud);
+        }
+
+        else if (users.isPresent()) {
+            Users existUsers = users.get();
+        }
+
+        // @TODO : JWT 토큰 발급
+        return null;
+    }
+
+    // @TODO : 구글, 애플 로그인
+    // @TODO : 프로필 사진 및 닉네임 정하는 API
 
     @DeleteMapping("/delete")
     public String delete(@RequestParam Long id) {
@@ -109,5 +144,11 @@ public class UsersController {
         }
 
         return result;
+    }
+
+    @GetMapping("/updateUsersInfoByUsersId")
+    public String updateUsersInfoByUsersId(@RequestParam Long usersId, String nickname, String profileImageUrl) {
+        usersService.updateUsersInfoByUsersId(usersId, nickname, profileImageUrl);
+        return usersId + "번 유저의 정보를 변경합니다.";
     }
 }
--- src/main/java/com/swm/ventybackend/users/UsersRepository.java
+++ src/main/java/com/swm/ventybackend/users/UsersRepository.java
@@ -51,11 +51,30 @@ public class UsersRepository {
 //        return users;
 //    }
 
+    public Optional<Users> findUsersByOAuthIdAndType(Long oAuthId, String oAuthType) {
+        List<Users> users = em.createQuery("SELECT users FROM Users users WHERE users.oAuthId = :oAuthId AND users.oAuthType = :oAuthType", Users.class)
+                .setParameter("oAuthId", oAuthId)
+                .setParameter("oAuthType", oAuthType)
+                .getResultList();
+        return users.stream().findAny();
+    }
+
     public void updateUsersToKakaoUsers(Long kakaoId, String email) {
         em.createQuery("UPDATE Users users SET users.usersId = :kakaoId WHERE users.email = :email")
                 .setParameter("kakaoId", kakaoId)
                 .setParameter("email", email)
                 .executeUpdate();
         em.clear();
+    }
+
+    // @TODO: ProfileImage 어떻게 관리?
+    public void updateUsersInfoByUsersId(Long usersId, String nickname, String profileImageUrl) {
+        em.createQuery("UPDATE Users users SET users.nickName = :nickname, users.profileImageUrl = :profileImageUrl WHERE users.usersId =: usersId")
+                .setParameter("usersId", usersId)
+                .setParameter("nickname", nickname)
+                .setParameter("profileImageUrl", profileImageUrl)
+                .executeUpdate();
+        em.clear();
+
     }
 }
